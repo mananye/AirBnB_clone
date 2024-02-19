2defb1ee86ef6fc21649d9407c99d00ae6518f96
mananye <nkwanagoden@gmail.com>
2024-02-19 11:55:02 +0000

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,31 +20,43 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    CLASSES = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Place": Place,
+        "Amenity": Amenity,
+        "Review": Review
+    }
+
     def all(self):
         """Return the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        serialized = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized[key] = obj.to_dict()
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+            json.dump(serialized, f)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+            with open(FileStorage.__file_path, "r") as f:
+                serialized = json.load(f)
+                for key, data in serialized.items():
+                    class_name = data["__class__"]
+                    cls = FileStorage.CLASSES[class_name]
+                    obj = cls(**data)
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
-            return
+            pass
 
